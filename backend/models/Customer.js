cb54378683d65a5e2e765422636c376dae20a3b5
ZeelGandhi123 <158592807+ZeelGandhi123@users.noreply.github.com>
2024-02-02 15:07:51 -0500

    added login api and functionality, password hashing, signup functionality

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Define Customer schema
 const customerSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    phoneNumber: String,
-    address: String
-  });
-  
-const Customer = mongoose.model('customer', customerSchema)
-
-module.exports = Customer
+  name: String,
+  email: String,
+  password: String,
+  phoneNumber: String,
+  address: String
+});
 
 
+
+const Customer = mongoose.model('customer', customerSchema);
+
+module.exports = Customer;
