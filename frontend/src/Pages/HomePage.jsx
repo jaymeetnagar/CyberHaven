5815d6d9115b1a7ca759ae1fbddfa1edacd29d2f
Harvinder Singh <gahir126@gmail.com>
2024-02-21 15:33:51 -0500

    Add product routes and product card component

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import { faCartArrowDown, faCartFlatbed, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./HomePage.css";
+import ProductCard from "../Components/ProductCard";
 export default function HomePage() {
   return (
     <div className="HomePage">
+
       <div className="main-section">
 
       </div>
+
       <div className="cart-section">
         <div className="cart-title">Cart <FontAwesomeIcon icon={faCartShopping} /></div>
       </div>
