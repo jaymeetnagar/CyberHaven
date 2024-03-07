51b7fea928317cd9df05ac48964624911384bd00
Harvinder Singh <Gahir126@gmail.com>
2024-03-07 15:18:35 -0500

    Update ProductCard component style

--- frontend/src/Components/ProductCard.js
+++ frontend/src/Components/ProductCard.js
@@ -1,41 +1,19 @@
 import React from 'react';
 
+/* Product Card */
+
 const ProductCard = ({ product }) => {
-  const cardStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '300px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    padding: '10px',
-    margin: '10px'
-  };
-
-  const imageStyle = {
-    width: '100%',
-    height: 'auto',
-    marginBottom: '10px'
-  };
-
-  const buttonStyle = {
-    backgroundColor: 'blue',
-    color: 'white',
-    border: 'none',
-    padding: '10px',
-    borderRadius: '5px',
-    cursor: 'pointer'
-  };
 
   return (
-    <div style={cardStyle}>
-      <img src="https://placehold.co/600x400/png" alt="Product Name" style={imageStyle} />
-      <div className="product-details">
-        <h2>{"Product Name"}</h2>
-        <p>${"499.00"}</p>
-        <button style={buttonStyle}>Add to Cart</button>
+    <article className="card" style={{ width: '18rem' }}>
+      <img src="https://placehold.co/600x400/png" className="card-img-top" alt="Product Name" />
+      <div className="card-body text-center">
+        <h5 className="card-title">Product Name</h5>
+        <p className="card-text">$499.00</p>
+        <button className="btn btn-info rounded-pill">Add to Cart</button>
       </div>
-    </div>
+    </article>
+
   );
 };
 
