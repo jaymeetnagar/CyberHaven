a856b65358fadadd4e6f1998468a53580f8f62cb
Keval9shah <kevalthedeveloper@gmail.com>
2024-03-08 18:23:55 -0500

    Add ProductCard.css and import it in ProductCard.js

--- frontend/src/Components/ProductCard.js
+++ frontend/src/Components/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ProductCard.css';
 
 /* Product Card */
 
@@ -7,19 +8,17 @@ const ProductCard = ({ product }) => {
 
   return (
     <article className="card mb-4">
-      <img src="https://placehold.co/600x400/png" className="card-img-top" alt={ product.name } />
+      <img src={product.imageURL} className="card-img-top" alt={product.name} />
       <div className="card-body text-center">
-        <h5 className="card-title">{ product.name }</h5>
-        <p className="card-text mb-2">${ (product.price).toFixed(2) }</p>
-        
-        <p className="small text-muted mb-3">{ product.category }</p>
-
-        <div>
-          
-         <Link className="btn btn-info rounded-pill text-uppercase me-1" to={ `/product-details/${product._id}` }>View Details</Link>
-         <button className="btn btn-info rounded-pill text-uppercase" product-id={ product._id }>Add to Cart</button>
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text mb-2">${(product.price).toFixed(2)}</p>
+        <p className="small text-muted mb-3">{product.category}</p>
+        <div className='mb-2'>
+          <Link className="btn btn-info rounded-pill text-uppercase me-1" to={`/product-details/${product._id}`}>View Details</Link>
         </div>
-
+        <div>
+          <button className="btn btn-info rounded-pill text-uppercase" product-id={product._id}>Add to Cart</button>
+        </div>
       </div>
     </article>
 
