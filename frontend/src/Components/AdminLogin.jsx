9eb426d544923c769f5c28a7088712dd6aec2a0e
Harvinder Singh <gahir126@gmail.com>
2024-04-07 15:21:44 -0400

    Use bootstrap for admin login page

--- frontend/src/Components/AdminLogin.jsx
+++ frontend/src/Components/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUserData, updateUserData } from "../store";
-import "./AdminLogin.css";
+//import "./AdminLogin.css";
 
 const AdminLogin = () => {
     const [email, setEmail] = useState("");
@@ -37,36 +37,48 @@ const AdminLogin = () => {
     };
 
     return (
-        <div className="admin-login-container">
-            <div className="admin-login-box">
-                <h2 className="mb-4">Admin Login</h2>
-                <form>
-                    <div className="form-group">
-                        <label>Email:</label>
+        <div className="container-fluid vh-100 d-flex justify-content-center align-items-center bg-dark">
+            <div className="col-md-6">
+
+                  <div className="card">
+                    <div className="card-header">
+                    <h2 className="text-center">Admin Login</h2>
+                    </div>
+                    <div className="card-body">
+
+                    <form>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email Address</label>
                         <input
                             type="text"
                             className="form-control"
-                            value={email}
+                            id="email"
+                            value={email} required
                             onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
-                    <div className="form-group">
-                        <label>Password:</label>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
                         <input
                             type="password"
                             className="form-control"
-                            value={password}
+                            id="password"
+                            value={password} required
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <button
                         type="button"
                         onClick={handleLogin}
-                        className="btn btn-primary"
+                        className="btn btn-info w-100"
                     >
                         Login
                     </button>
                 </form>
+                    </div>
+
+                  </div>
+
                 {errorMessage && (
                     <p className="error-message">{errorMessage}</p>
                 )}
