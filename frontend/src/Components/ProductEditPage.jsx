c898afa7d68c711bc5b2ed8f672707d75539fc47
Keval9shah <kevalthedeveloper@gmail.com>
2024-04-15 18:43:30 -0400

    product update bug

--- frontend/src/Components/ProductEditPage.jsx
+++ frontend/src/Components/ProductEditPage.jsx
@@ -1,44 +1,36 @@
-import { Link, useParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Alert from './Alert';
+import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Alert from "./Alert";
 
 const ProductEditPage = () => {
-
     const { productId } = useParams();
-
-    const [product, setProduct] = useState([]);
-
+    const [product, setProduct] = useState({});
     const [showAlert, setShowAlert] = useState(false);
 
-    useEffect(() => { fetchProductData(); }, []);
+    useEffect(() => {
+        fetchProductData();
+    }, []);
 
     const fetchProductData = async () => {
         try {
             const response = await axios.get(`http://localhost:3001/product/${productId}`);
             setProduct(response.data.data);
         } catch (error) {
-            console.error('Error fetching data:', error);
+            console.error("Error fetching data:", error);
         }
     };
 
-
     const handleSubmit = (event) => {
-        
         event.preventDefault();
 
-        const editedProduct = {
-            productId : productId,
+        const newProductData = {
             title: event.target.title.value,
-            price: event.target.price.value,
+            price: parseInt(event.target.price.value),
             category: event.target.category.value,
             description: event.target.description.value,
-            imageURL: event.target.imageURL.value
+            imageURL: event.target.imageURL.value,
         };
-        
-        // Logic to handle form submission
-        console.log('Edited product:', editedProduct);
-
 
         fetch(`http://localhost:3001/product`, {
             method: "PUT",
@@ -46,87 +38,81 @@ const ProductEditPage = () => {
                 "Content-Type": "application/json",
             },
             credentials: "include",
-            body: JSON.stringify(editedProduct)
+            body: JSON.stringify({productId, newProductData}),
         })
-        .then((response) => response.json())
-        .then((result) => {
-
-            setShowAlert(true);
-
-        });
-
+            .then((response) => response.json())
+            .then((result) => {
+                setShowAlert(true);
+            });
     };
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
 
-
+        setProduct({
+            ...product,
+            [name]: value
+        });
+    };
 
     return (
-
-
-        <div className='bg-dark vh-min-100'>
-
-        <div className="container">
-            <h1 className="mb-3 text-center text-white pt-4">CyberHaven - Admin</h1>
-
-            
-            { showAlert && <Alert message={ "Product Updated Successfully" } /> }
-
-
-           <div className='row'>
-
-            <div className="col-md-5 mb-3">
-            <div className="mb-4">
-                
-                <h3 className='mb-3 text-white'>Edit Product</h3>
-                
-                <Link to="/admin/" className='btn btn-light'>Back to Admin</Link>
-                
+        <div className="bg-dark vh-min-100">
+            <div className="container">
+                <h1 className="mb-3 text-center text-white pt-4">CyberHaven - Admin</h1>
+                {showAlert && <Alert message={"Product Updated Successfully"} />}
+                <div className="row">
+                    <div className="col-md-5 mb-3">
+                        <div className="mb-4">
+                            <h3 className="mb-3 text-white">Edit Product</h3>
+                            <Link to="/admin/" className="btn btn-light">
+                                Back to Admin
+                            </Link>
+                        </div>
+                        <img src={product.imageURL} className="rounded img-thumbnail" alt={product.title} />
+                    </div>
+                    <div className="col-md-7">
+                        <div className="card mb-5">
+                            <div className="card-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="title" className="form-label">
+                                            Title
+                                        </label>
+                                        <input type="text" onChange={handleInputChange} className="form-control bg-white text-body" id="title" name="title" value={product.title} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="price" className="form-label">
+                                            Price
+                                        </label>
+                                        <input type="number" onChange={handleInputChange} className="form-control bg-white text-body" id="price" name="price" value={product.price} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="category" className="form-label">
+                                            Category
+                                        </label>
+                                        <input type="text" onChange={handleInputChange} className="form-control bg-white text-body" id="category" name="category" value={product.category} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="imageURL" className="form-label">
+                                            Image
+                                        </label>
+                                        <input type="text" onChange={handleInputChange} className="form-control bg-white text-body" id="imageURL" name="imageURL" value={product.imageURL} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="description" className="form-label">
+                                            Description
+                                        </label>
+                                        <textarea className="form-control bg-white text-body" id="description" rows={6} name="description" value={product.description} required onChange={handleInputChange} />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">
+                                        Update Product
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <img src={product.imageURL} className="rounded img-thumbnail" alt={product.title} />
             </div>
-            <div className="col-md-7">
-                
-                <div className='card mb-5'>
-                    <div className='card-body'>
-
-                    <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control bg-white text-body" id="title" name="title" value={product.title}  required />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="price" className="form-label">Price</label>
-                        <input type="number" className="form-control bg-white text-body" id="price" name="price" value={product.price}  required />
-                    </div>
-                    
-                    <div className="mb-3">
-                        <label htmlFor="category" className="form-label">Category</label>
-                        <input type="text" className="form-control bg-white text-body" id="category" name="category" value={product.category}  required />
-                    </div>
-
-
-                    <div className="mb-3">
-                        <label htmlFor="imageURL" className="form-label">Image</label>
-                        <input type="text" className="form-control bg-white text-body" id="imageURL" name="imageURL" value={product.imageURL}  required />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="description" className="form-label">Description</label>
-                        <textarea className="form-control bg-white text-body" id="description" rows={6} name="description" value={product.description}  required />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Update Product</button>
-                </form>
-
-                    </div>
-
-                </div>
-
-
-            </div>
-            </div>
-
-        </div>
-
         </div>
     );
 };
